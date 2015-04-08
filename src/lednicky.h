79a68596b2f0459a8ddfa6524010b18566789985
Andrew Kubera <andrewkubera@gmail.com>
2015-04-08 12:00:03 +0200

    Added a structure to hold Lednicky equation parameters.

--- src/lednicky.h
+++ src/lednicky.h
@@ -1,17 +1,53 @@
+///
+/// \file lednicky.h
+/// \brief Main include file for LednickyEqn
+///
 
+#pragma once
 
 #include <TGraph.h>
 
-extern bool identical;  //Are the two particles identical?  This turns on/off quantum interference
+typedef unsigned short ushort_t;
+typedef struct LednikcyEquation LednikcyEquation_s;
+
+struct LednikcyEquation {
+  /// Are the two particles identical?  This turns on/off quantum interference
+  bool identical;
+
+  /// Number of bins to give created histograms
+  ushort_t totalBins;
+
+  /// Normalization Factor
+  double normalization;
+
+  /// Lambda Parameter for primary pairs
+  double lamPrimary;
+
+  /// Maximum k* on histograms
+  double maxKstar;
+
+  /// Femtoscopic Radius
+  double radius;
+
+  /// Effective range of interaction (should be >= 0)
+  double d0;
+
+  /// Real part of f0
+  double f0re;
+
+  /// Imaginary part of f0
+  double f0im;
+};
+
+extern bool identical;  //
 extern int totalBins; //How many bins will the histograms have?  maxKstar/totalBins will be the bin width.
 extern double normalization;  //Simple normalization factor
 extern double lamPrimary; // lambda parameter for primary pairs
 extern double maxKstar;
 extern double radius;
-extern double d0; // Effective range of interaction (Should be >= 0)
+extern double d0; //
 extern double f0re;
 extern double f0im;
 
 
 TGraph* GetLednickyEqn(bool identicalParticles);
-
